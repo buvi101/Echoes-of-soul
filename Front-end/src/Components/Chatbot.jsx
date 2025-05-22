786475d33a8ef14f1b6b6c87b2116b5d041d81e8
Bhuwan prasad <147224962+buvi101@users.noreply.github.com>
2025-05-22 11:41:59 +0530

    Update Chatbot.jsx

--- Front-end/src/Components/Chatbot.jsx
+++ Front-end/src/Components/Chatbot.jsx
@@ -40,7 +40,7 @@ const Chatbot = () => {
 
   const generateBotReply = async (userInput) => {
     try {
-      const res = await fetch("http://127.0.0.1:5000/chat", {
+      const res = await fetch("https://echoes-of-soul-chatbot-env.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
